242f3869c872d6a1b8dc5637a08b4ce619b8a2c9
NielsBij <nvbijsterveldt@hotmail.com>
2021-09-20 15:44:20 +0200

    Added a new project for PoC app

---
+++ SSOAssignmentPoC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SSOAssignmentPoC.MvcApplication" Language="C#" %>
---
+++ SSOAssignmentPoC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SSOAssignmentPoC.Startup))]
+namespace SSOAssignmentPoC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
