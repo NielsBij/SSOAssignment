a3b29012d84fb4f3c4812be46eede386ac2c944d
NielsBij <nvbijsterveldt@hotmail.com>
2021-09-20 10:24:09 +0200

    Initial

---
+++ SSOAssignment/SSOAssignment/Controllers/MainPageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSOAssignment.Controllers
+{
+    public class MainPageController : Controller
+    {
+        // GET: MainPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string MainAppPage()
+        {
+            return "This is the main page";
+        }
+
+        public string InitialTest(string name)
+        {
+            return "Test succesful," + name;
+        }
+    }
+}
